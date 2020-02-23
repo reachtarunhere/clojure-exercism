31fb766314cae4c12b781fa3e570e2f27021a9c0
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 00:18:49 +0800

    nicer solution with iterate and take-while

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,10 +1,9 @@
 (ns collatz-conjecture)
 
+(defn- step [x] (if (even? x) (/ x 2) (+ 1 (* 3 x))))
+
 (defn collatz [num] ;; <- arglist goes here
   (if (< num 1)
     (throw (Throwable. "num < 1 invalid."))
-    (let [collatz-tail (fn [num ans]
-                         (if (= num 1)
-                           ans
-                           (recur (if (odd? num) (+ 1 (* 3 num)) (/ num 2)) (+ 1 ans))))]
-      (collatz-tail num 0))))
+    (count (take-while #(> % 1) (iterate step num)))))
+
