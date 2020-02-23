e6b1f874c142b920ed9fb412a1461fefe8ffc7fa
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-23 13:35:35 +0800

    used = for equal case instead of sublisting in both directions

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -6,10 +6,9 @@
     (defn equal-starting-from [start] (= (take (count a) (drop start b)) a))
     (or (= a []) (some true? (map equal-starting-from (range 0 (count b))))))
 
-  (let [l1-sub-l2 (is-sublist-of list1 list2) l2-sub-l1 (is-sublist-of list2 list1)]
     (cond
-      (and l1-sub-l2 l2-sub-l1) :equal
-      l1-sub-l2 :sublist
-      l2-sub-l1 :superlist
-      :else :unequal))
+      (= list1 list2) :equal
+      (is-sublist-of list1 list2) :sublist
+      (is-sublist-of list2 list1)  :superlist
+      :else :unequal)
 )
