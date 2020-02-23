cf0b44c5a5c89d0defcddb111f7ae7a14d5157bd
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 01:02:15 +0800

    define individual nucleotide function in terms of all coounts fn

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -1,10 +1,5 @@
 (ns nucleotide-count)
 
-(defn count-of-nucleotide-in-strand [nucleotide strand] ;; <- Arglist goes here
-  (if (some #{nucleotide} [\A \T \C \G])
-    (apply + (map #(if (= nucleotide %) 1 0) strand))
-    (throw (Throwable. "Bad Nucleotide"))))
-
 (defn- increment-key
   ;; also just adds the key if not found to avoid failiure in case of strand with unknown character
   [map key]
@@ -13,3 +8,8 @@
 (defn nucleotide-counts [strand] ;; <- Arglist goes here
   (reduce increment-key {\A 0 \T 0 \C 0 \G 0} strand)
 )
+
+(defn count-of-nucleotide-in-strand [nucleotide strand] ;; <- Arglist goes here
+  (if (some #{nucleotide} [\A \T \C \G])
+    ((nucleotide-counts strand) nucleotide)
+    (throw (Throwable. "Bad Nucleotide"))))
