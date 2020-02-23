fd4ba8eb939278f9c1460ac9419ae0a6130c07d7
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 02:29:40 +0800

    slightly but not so nicer with filter

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -2,5 +2,5 @@
 
 (defn distance [strand1 strand2] ; <- arglist goes herex
   (if (= (count strand1) (count strand2))
-    (apply + (map #(if (= %1 %2) 0 1) strand1 strand2)))
+    (count (filter true? (map not= strand1 strand2))))
 )
