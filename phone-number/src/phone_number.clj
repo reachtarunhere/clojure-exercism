1a25563d8742a68182caba7fbc9c4a0de85392b8
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 05:36:40 +0800

    merge validate and number functions

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -3,22 +3,14 @@
 (defn- only-numeric-chars [num-str]
   (filter #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9} num-str))
 
-(defn- validate-num-only-str [num-only-str]
-  (or
-   (= 10 (count num-only-str))
-   (and (= (count num-only-str) 11) (= \1 (first num-only-str)))))
-
-
-(defn number [num-string] ;; <- arglist goes here
-  (let [num-only-str (only-numeric-chars num-string)
-        is-valid (validate-num-only-str num-only-str)]
+(defn number [num-string]
+  (let [num-only-str (only-numeric-chars num-string)] 
     (clojure.string/join
-     (if is-valid
-       (take-last 10 num-only-str)
-       (repeat 10 0)))
-    )    
-)
-
+     (cond
+       (= 10 (count num-only-str)) num-only-str
+       (and (= (count num-only-str) 11) (= \1 (first num-only-str))) (take-last 10 num-only-str)
+       :else (repeat 10 0)))))
+  
 (defn area-code [num-string] ;; <- arglist goes here
   (clojure.string/join (take 3 (number num-string)))
 )
