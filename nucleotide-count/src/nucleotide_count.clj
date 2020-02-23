fcfa233db96b0d6c13ba8eeea37ff2dbeb5f417f
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 00:57:13 +0800

    handle bad nucleotides while incrementing

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -6,8 +6,9 @@
     (throw (Throwable. "Bad Nucleotide"))))
 
 (defn- increment-key
+  ;; also just adds the key if not found to avoid failiure in case of strand with unknown character
   [map key]
-  (assoc map key (inc (map key))))
+  (assoc map key (inc (get map key 0))))
 
 (defn nucleotide-counts [strand] ;; <- Arglist goes here
   (reduce increment-key {\A 0 \T 0 \C 0 \G 0} strand)
