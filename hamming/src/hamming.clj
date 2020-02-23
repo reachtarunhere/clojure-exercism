5b277fd430d0912b44f3ad9122a4b8ee8067baf5
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 02:23:29 +0800

    if with only one branch returns nil otherwise

--- hamming/src/hamming.clj
+++ hamming/src/hamming.clj
@@ -1,7 +1,6 @@
 (ns hamming)
 
 (defn distance [strand1 strand2] ; <- arglist goes herex
-  (if (not= (count strand1) (count strand2))
-    nil
+  (if (= (count strand1) (count strand2))
     (apply + (map #(if (= %1 %2) 0 1) strand1 strand2)))
 )
