7ae16333ce682fa0ffb4fcaa3d3b9eff3853c018
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 01:21:19 +0800

    use built in frequencies instead of implementing own counter

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -1,13 +1,7 @@
 (ns nucleotide-count)
 
-(defn- increment-key
-  ;; also just adds the key if not found to avoid failiure in case of strand with unknown character
-  [map key]
-  (assoc map key (inc (get map key 0))))
-
 (defn nucleotide-counts [strand] ;; <- Arglist goes here
-  (reduce increment-key {\A 0 \T 0 \C 0 \G 0} strand)
-)
+  (merge {\A 0 \T 0 \C 0 \G 0} (frequencies strand)))
 
 (defn count-of-nucleotide-in-strand [nucleotide strand] ;; <- Arglist goes here
   {:pre [(#{\A \T \C \G} nucleotide)]}
