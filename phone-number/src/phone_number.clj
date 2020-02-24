4179c4e8976010bcca8b415794105d49c0025e85
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-24 15:26:43 +0800

    use subs, format and import join for cleaner version

--- phone-number/src/phone_number.clj
+++ phone-number/src/phone_number.clj
@@ -1,25 +1,26 @@
-(ns phone-number)
+(ns phone-number
+  (:use [clojure.string :only [join]]))  
 
 (defn- only-numeric-chars [num-str]
   (filter #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9} num-str))
 
 (defn number [num-string]
   (let [num-only-str (only-numeric-chars num-string)] 
-    (clojure.string/join
+    (join
      (cond
        (= 10 (count num-only-str)) num-only-str
        (and (= (count num-only-str) 11) (= \1 (first num-only-str))) (take-last 10 num-only-str)
        :else (repeat 10 0)))))
   
 (defn area-code [num-string] ;; <- arglist goes here
-  (clojure.string/join (take 3 (number num-string)))
+  (join (take 3 (number num-string)))
 )
 
 (defn pretty-print [num-string] ;; <- arglist goes here
   (let [ac (area-code num-string)
         full-no (number num-string)
-        ec (clojure.string/join (take 3 (drop 3 full-no)))
-        sn (clojure.string/join (take-last 4 full-no))
+        ec (subs full-no 3 6)
+        sn (subs full-no 6)
         ]
-    (str "(", ac, ") ", ec, "-", sn))
+    (format "(%s) %s-%s" ac ec sn))
 )
