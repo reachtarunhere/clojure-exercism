8760f354209ed68f0cd21ff429fd0556fd81d329
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-25 08:46:53 +0800

    first pass on clock

---
+++ clock/src/clock.clj
@@ -0,0 +1,16 @@
+(ns clock)
+
+(defn clock->string [clock] ;; <- arglist goes here
+    (format "%02d:%02d" (first clock) (second clock))
+)
+
+(defn clock [hours minutes] ;; <- arglist goes here
+  (let [total-mins (mod (+ (* 60 hours) minutes)  (* 60 24))
+        real-hrs (mod (quot total-mins 60) 24)
+        real-mins (mod total-mins 60)]
+    (list real-hrs real-mins))
+)
+
+(defn add-time [clock-tuple time] ;; <- arglist goes here
+  (clock (first clock-tuple) (+ time (second clock-tuple)))
+)
