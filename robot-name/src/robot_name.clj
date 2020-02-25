14171daf738067c57e386363d26b4d026ce3e172
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-25 19:04:05 +0800

    use atoms as a way to maintains state. also added unique name check

--- robot-name/src/robot_name.clj
+++ robot-name/src/robot_name.clj
@@ -1,22 +1,24 @@
 (ns robot-name)
  
-(def all-robo-list [])
+(def all-robo-list (atom []))
 
-(defn generate-robo-name []
+(defn generate-name-candidate []
   (str
    (apply str (repeatedly 2 #(rand-nth (map char (range 65 91)))))
    (rand-nth (range 100 1000))))
 
+(defn unique-name []
+  (first (drop-while (fn [cand] (#{cand} @all-robo-list)) (repeatedly generate-name-candidate))))
 
 (defn robot [] ;; <- arglist goes here
   (do
-    (def all-robo-list (conj all-robo-list (generate-robo-name)))
-    (dec (count all-robo-list))))
+    (swap! all-robo-list #(conj % (unique-name)))
+    (dec (count @all-robo-list))))
      
 (defn robot-name [robot] ;; <- arglist goes here
-  (nth all-robo-list robot)
+  (nth @all-robo-list robot)
 )
 
 (defn reset-name [robot] ;; <- arglist goes here
-    (def all-robo-list (assoc all-robo-list robot (generate-robo-name)))
+    (swap! all-robo-list #(assoc % robot (unique-name)))
 )
