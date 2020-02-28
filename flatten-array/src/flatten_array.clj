f21ac6d7b79ae056b9ba87b6cee759ba8a9dd5a5
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-28 22:54:51 +0800

    implemented using tree-seq for depth first search

--- flatten-array/src/flatten_array.clj
+++ flatten-array/src/flatten_array.clj
@@ -1,18 +1,5 @@
 (ns flatten-array)
 
-(defn flatten-tco [arr output]
-  (if (empty? arr)
-    output
-    (let [hd (first arr) tl (rest arr)]
-      (if (coll? hd)
-        (recur (concat hd tl) output)
-        (recur tl (concat output [hd])))))
-  
-)
-
-
-
-
 (defn flatten [arr] ;; <- arglist goes here
-  (filter some? (flatten-tco arr '()))
+  (filter number? (tree-seq coll? identity arr))
 )
