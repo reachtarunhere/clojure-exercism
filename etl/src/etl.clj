0ef278127085814e34c17aef6eb96288824aed37
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-29 00:18:03 +0800

    take out anon function for clear code

--- etl/src/etl.clj
+++ etl/src/etl.clj
@@ -1,9 +1,10 @@
 (ns etl
   (:use [clojure.string :only [lower-case]]))
 
+(defn transpose [[point letters]]
+  (zipmap (map lower-case letters) (repeat point))
+
 (defn transform [source] ;; <- arglist goes here
-  (reduce (fn [out [point letters]] (into out (zipmap (map lower-case letters) (repeat (count letters) point))))
-          {}
-          source)
+  (reduce #(into %1 (tranpose %2)) {} source)
   )
    
